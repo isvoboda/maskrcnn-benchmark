854b49bf1e44cb0916d9a08309b93e851a663593
Pavel Svoboda <pavel.svoboda@innovatrics.com>
2019-11-19 14:45:16 +0000

    Add user evaluation based on COCO
    [IMAGE-1817]

--- demo/eval_user_coco.py
+++ demo/eval_user_coco.py
@@ -18,149 +18,115 @@ class ARGS(NamedTuple):
 
 args_real_reported = ARGS(
     "datasets/pcards/annotations/pcards-real-00-test.json",
-    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_real_00_test/bbox.json"
+    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_real_00_test/bbox.json",
+)
+
+# ---------------------------- M06 -------------------------------
+args_M06_coco_pcards_synth_01_train_defects_only_class_type = ARGS(
+    "datasets/pcards/annotations/pcards-synth-01-train-defects-only-class-type.json",
+    "models/06-inn-pcards-04-real_labeled_loss_weights/inference/coco_pcards_synth_01_train_defects_only_class_type/bbox.json",
+)
+args_M06_coco_pcards_real_00_test_defects_only_class_type = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test-defects-only-class-type.json",
+    "models/06-inn-pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_test_defects_only_class_type/bbox.json",
+)
+args_M06_coco_pcards_real_00_test = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test.json",
+    "models/06-inn-pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_test/bbox.json",
+)
+
+# ---------------------------- M07 -------------------------------
+args_M07_coco_pcards_real_00_train_defects_only_class_type = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-train-defects-only-class-type.json",
+    "models/07-pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_train_defects_only_class_type/bbox.json",
+)
+args_M07_coco_pcards_real_00_test_defects_only_class_type = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test-defects-only-class-type.json",
+    "models/07-pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_test_defects_only_class_type/bbox.json",
+)
+args_M07_coco_pcards_real_00_test = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test.json",
+    "models/07-pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_test/bbox.json",
+)
+
+# ---------------------------- M08 -------------------------------
+args_M08_coco_pcards_synth_01_train_defects_only_class_type = ARGS(
+    "datasets/pcards/annotations/pcards-synth-01-train-defects-only-class-type.json",
+    "models/08-pcards-04-synth_labeled_loss_weights/inference/coco_pcards_synth_01_train_defects_only_class_type/bbox.json",
+)
+args_M08_coco_pcards_real_00_test_defects_only_class_type = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test-defects-only-class-type.json",
+    "models/08-pcards-04-synth_labeled_loss_weights/inference/coco_pcards_real_00_test_defects_only_class_type/bbox.json",
+)
+args_M08_coco_pcards_real_00_test = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test.json",
+    "models/08-pcards-04-synth_labeled_loss_weights/inference/coco_pcards_real_00_test/bbox.json",
+)
+
+# ----------------------------------------------------------------
+
+args_real_00_test_labeled = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-test-defects-labeled.json",
+    "models/pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_test_labeled/bbox.json",
+)
+
+args_real_00_train_labeled = ARGS(
+    "datasets/pcards/annotations/pcards-real-00-train-defects-labeled.json",
+    "models/pcards-04-real_labeled_loss_weights/inference/coco_pcards_real_00_train_labeled/bbox.json",
 )
 
 args_synth_01 = ARGS(
     "datasets/pcards/annotations/test-type-1-b-synth-01.json",
-    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test/bbox.json"
+    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test/bbox.json",
 )
 
 args_synth_01_ellipse = ARGS(
     "datasets/pcards/annotations/test-type-1-b-synth-01-ellipse.json",
-    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test-ellipse/bbox.json"
+    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test-ellipse/bbox.json",
 )
 
 args_synth_01_line = ARGS(
     "datasets/pcards/annotations/test-type-1-b-synth-01-line.json",
-    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test-line/bbox.json"
+    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test-line/bbox.json",
 )
 
 args_synth_01_arc = ARGS(
     "datasets/pcards/annotations/test-type-1-b-synth-01-arc.json",
-    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test-arc/bbox.json"
+    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_01_test-arc/bbox.json",
 )
 
 
 args_synthetic = ARGS(
     "datasets/pcards/annotations/pcards-synthetic-00-val-poly-filtered.json",
-    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_00_val/bbox.json"
+    "models/pcards-04-real_loss_weights_b/inference/coco_pcards_synthetic_00_val/bbox.json",
 )
 
 args_iou = ARGS()
 
 args_iou_template_synthetic = ARGS(
     "datasets/pcards/annotations/pcards-synthetic-00-val-poly-filtered.json",
-    "models/pcards-03-iou-template/inference/coco_pcards_synthetic_00_val/bbox-filtered.json"
+    "models/pcards-03-iou-template/inference/coco_pcards_synthetic_00_val/bbox-filtered.json",
 )
 
 args_iou_template_real = ARGS(
     "datasets/pcards/annotations/pcards-real-00-test.json",
-    "models/pcards-03-iou-template/inference/coco_pcards_real_00_test/bbox.json"
+    "models/pcards-03-iou-template/inference/coco_pcards_real_00_test/bbox.json",
 )
 
 
 args_iou_template_real_filtered = ARGS(
     "datasets/pcards/annotations/pcards-real-00-test-filtered.json",
-    "models/pcards-03-iou-template/inference/coco_pcards_real_00_test/bbox-filtered.json"
+    "models/pcards-03-iou-template/inference/coco_pcards_real_00_test/bbox-filtered.json",
 )
 
-args = args_synth_01_arc
+args = args_M08_coco_pcards_synth_01_train_defects_only_class_type
 dt = COCO(args.dataset)
 res = dt.loadRes(args.inference)
 
-
-def summarize(self):
-    '''
-    Compute and display summary metrics for evaluation results.
-    Note this functin can *only* be applied on the default parameter setting
-    '''
-    def _summarize(ap=1, iouThr=None, areaRng='all', maxDets=100):
-        p = self.params
-        iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
-        titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
-        typeStr = '(AP)' if ap == 1 else '(AR)'
-        iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
-            if iouThr is None else '{:0.2f}'.format(iouThr)
-
-        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
-        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
-        if ap == 1:
-            # dimension of precision: [TxRxKxAxM]
-            s = self.eval['precision']
-            # IoU
-            if iouThr is not None:
-                t = np.where(iouThr == p.iouThrs)[0]
-                s = s[t]
-            s = s[:, :, :, aind, mind]
-        else:
-            # dimension of recall: [TxKxAxM]
-            s = self.eval['recall']
-            if iouThr is not None:
-                t = np.where(iouThr == p.iouThrs)[0]
-                s = s[t]
-            s = s[:, :, aind, mind]
-        if len(s[s > -1]) == 0:
-            mean_s = -1
-        else:
-            mean_s = np.mean(s[s > -1])
-        print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
-        return mean_s
-
-    def _summarizeDets():
-        stats = np.zeros((12,))
-        stats[0] = _summarize(1)
-        stats[1] = _summarize(1, iouThr=.15, maxDets=self.params.maxDets[2])
-        stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-        stats[3] = _summarize(1, areaRng='small',
-                              maxDets=self.params.maxDets[2])
-        stats[4] = _summarize(1, areaRng='medium',
-                              maxDets=self.params.maxDets[2])
-        stats[5] = _summarize(1, areaRng='large',
-                              maxDets=self.params.maxDets[2])
-        stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-        stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-        stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-        stats[9] = _summarize(0, areaRng='small',
-                              maxDets=self.params.maxDets[2])
-        stats[10] = _summarize(0, areaRng='medium',
-                               maxDets=self.params.maxDets[2])
-        stats[11] = _summarize(
-            0, areaRng='large', maxDets=self.params.maxDets[2])
-        return stats
-
-    def _summarizeKps():
-        stats = np.zeros((10,))
-        stats[0] = _summarize(1, maxDets=20)
-        stats[1] = _summarize(1, maxDets=20, iouThr=.5)
-        stats[2] = _summarize(1, maxDets=20, iouThr=.75)
-        stats[3] = _summarize(1, maxDets=20, areaRng='medium')
-        stats[4] = _summarize(1, maxDets=20, areaRng='large')
-        stats[5] = _summarize(0, maxDets=20)
-        stats[6] = _summarize(0, maxDets=20, iouThr=.5)
-        stats[7] = _summarize(0, maxDets=20, iouThr=.75)
-        stats[8] = _summarize(0, maxDets=20, areaRng='medium')
-        stats[9] = _summarize(0, maxDets=20, areaRng='large')
-        return stats
-    if not self.eval:
-        raise Exception('Please run accumulate() first')
-    iouType = self.params.iouType
-    if iouType == 'segm' or iouType == 'bbox':
-        summarize = _summarizeDets
-    elif iouType == 'keypoints':
-        summarize = _summarizeKps
-    self.stats = summarize()
-
-
-ceval = COCOeval(dt, res, "bbox")
-ceval.params.useCats = 0
-ceval.params.iouThrs = np.linspace(0.15, 0.95, 18)
-setattr(ceval, "summarize", summarize)
-
+iouThrs = np.linspace(0.15, 0.95, np.round((0.95 - 0.15) / 0.05) + 1, endpoint=True)
+ceval = COCOeval(dt, res, "bbox", iouThrs=iouThrs)
+ceval.params.useCats = 1
 #%%
 ceval.evaluate()
 ceval.accumulate()
-ceval.summarize(ceval)
-
-
-# %%
+ceval.user_summarize()
